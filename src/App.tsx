d84a186c64c4b12b538d6911a6300d32cde432f1
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-05-19 10:46:10 +0530

    feat: Store background URL in localStorage and update state initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Time from "./Components/Time/Time";
 import Todo from "./Components/Todo/Todo";
@@ -9,9 +9,10 @@ import Bookmarks from "./Components/Bookmarks/Bookmarks";
 import { Image } from "lucide-react";
 
 function App() {
-  const [bgUrl, setBgUrl] = useState(
-    "https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-  );
+  const [bgUrl, setBgUrl] = useState(() => {
+    const storedBgUrl = localStorage.getItem("bgUrl");
+    return storedBgUrl || "https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  });
   const [showModal, setShowModal] = useState(false);
   const [inputUrl, setInputUrl] = useState("");
 
@@ -25,6 +26,10 @@ function App() {
       setShowModal(false);
     }
   };
+
+  useEffect(() => {
+    localStorage.setItem("bgUrl", bgUrl);
+  }, [bgUrl]);
 
   return (
     <>
