286627273bfc94f67fe8ed462c7acd1bcd84511e
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-05-19 11:20:53 +0530

    Size

--- src/App.tsx
+++ src/App.tsx
@@ -187,7 +187,7 @@ function App() {
                   htmlFor="bg-file-input"
                   className="block text-sm font-medium text-gray-300 mb-1"
                 >
-                  Upload Image/Video &lt; 4MB from Device
+                  Upload Image/Video from Device <span className="text-red-500">{`( Size < 4MB )`}</span>
                 </label>
                 <input
                   id="bg-file-input"
